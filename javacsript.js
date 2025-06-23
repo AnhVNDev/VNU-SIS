34998d62c6ebe1870b792d4c184ab921f8b2ea4a
anhvn <anhvn@anhvn.com>
2025-06-23 17:31:17 +0700

    Add VNU SIS

---
+++ javacsript.js
@@ -0,0 +1,190 @@
+// carouselElement
+// const carouselElement = document.querySelector("#eventCarousel");
+// if(carouselElement) {
+//   const carouselIndicators = document.querySelectorAll(
+//     ".custom-indicators button"
+//   );
+
+//   const carousel = new bootstrap.Carousel(carouselElement, {
+//     interval: 1000,
+//     wrap: true,
+//   });
+
+//   carouselElement.addEventListener("slide.bs.carousel", (event) => {
+//     carouselIndicators.forEach((indicator) =>
+//       indicator.classList.remove("active")
+//     );
+//     carouselIndicators[event.to].classList.add("active");
+//   });
+// }
+
+// side menu ---------------------------
+const menuItems = document.querySelectorAll(".side-menu .menu-item");
+if (menuItems) {
+  menuItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      item.classList.toggle("active");
+      const targetId = item.getAttribute("data-target");
+      const subMenu = document.getElementById(targetId);
+      subMenu.classList.toggle("open");
+    });
+  });
+}
+
+// navbar
+function toggleSubmenu(event, submenuId) {
+  event.preventDefault();
+  const navItem = event.target.closest(".nav-item");
+  const allNavItems = document.querySelectorAll(".nav-item");
+
+  allNavItems.forEach((item) => {
+    if (item !== navItem) {
+      item.classList.remove("show");
+      const link = item.querySelector(".nav-link");
+      if (link) link.classList.remove("active");
+    }
+  });
+
+  navItem.classList.toggle("show");
+  const link = navItem.querySelector(".nav-link");
+  if (link) link.classList.toggle("active");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  jsPlumb.ready(function () {
+    jsPlumb.setContainer("orgchart");
+    // Kết nối sơ đồ
+    jsPlumb.connect({
+      source: "danguy",
+      target: "hoidong",
+      anchors: ["Bottom", "Top"],
+      endpoint: "Blank",
+      connector: "Flowchart",
+      paintStyle: { stroke: "#003366", strokeWidth: 1 },
+    });
+    jsPlumb.connect({
+      source: "danguy",
+      target: "bangiamhieu",
+      anchors: ["Bottom", "Top"],
+      endpoint: "Blank",
+      connector: "Flowchart",
+      paintStyle: { stroke: "#003366", strokeWidth: 1 },
+    });
+    jsPlumb.connect({
+      source: "danguy",
+      target: "tochucdoanthe",
+      anchors: ["Bottom", "Top"],
+      endpoint: "Blank",
+      connector: "Flowchart",
+      paintStyle: { stroke: "#003366", strokeWidth: 1 },
+    });
+    jsPlumb.connect({
+      source: "bangiamhieu",
+      target: "donvikh",
+      anchors: ["Bottom", "Top"],
+      endpoint: "Blank",
+      connector: "Flowchart",
+      paintStyle: { stroke: "#003366", strokeWidth: 1 },
+    });
+    jsPlumb.connect({
+      source: "bangiamhieu",
+      target: "khoachuyenmon",
+      anchors: ["Bottom", "Top"],
+      endpoint: "Blank",
+      connector: "Flowchart",
+      paintStyle: { stroke: "#003366", strokeWidth: 1 },
+    });
+    jsPlumb.connect({
+      source: "bangiamhieu",
+      target: "phongchucnang",
+      anchors: ["Bottom", "Top"],
+      endpoint: "Blank",
+      connector: "Flowchart",
+      paintStyle: { stroke: "#003366", strokeWidth: 1 },
+    });
+    jsPlumb.connect({
+      source: "donvikh",
+      target: "trungtam",
+      anchors: ["Bottom", "Top"],
+      endpoint: "Blank",
+      connector: "Flowchart",
+    });
+    jsPlumb.connect({
+      source: "donvikh",
+      target: "phongtn",
+      anchors: ["Bottom", "Top"],
+      endpoint: "Blank",
+      connector: "Flowchart",
+    });
+    jsPlumb.connect({
+      source: "trungtam",
+      target: "tapchi",
+      anchors: ["Bottom", "Top"],
+      endpoint: "Blank",
+      connector: "Flowchart",
+    });
+    jsPlumb.connect({
+      source: "khoachuyenmon",
+      target: "k1",
+      anchors: ["Bottom", "Top"],
+      endpoint: "Blank",
+      connector: "Flowchart",
+    });
+    jsPlumb.connect({
+      source: "khoachuyenmon",
+      target: "k2",
+      anchors: ["Bottom", "Top"],
+      endpoint: "Blank",
+      connector: "Flowchart",
+    });
+    jsPlumb.connect({
+      source: "khoachuyenmon",
+      target: "k3",
+      anchors: ["Bottom", "Top"],
+      endpoint: "Blank",
+      connector: "Flowchart",
+    });
+    jsPlumb.connect({
+      source: "khoachuyenmon",
+      target: "k4",
+      anchors: ["Bottom", "Top"],
+      endpoint: "Blank",
+      connector: "Flowchart",
+    });
+    jsPlumb.connect({
+      source: "phongchucnang",
+      target: "p1",
+      anchors: ["Bottom", "Top"],
+      endpoint: "Blank",
+      connector: "Flowchart",
+    });
+    jsPlumb.connect({
+      source: "phongchucnang",
+      target: "p2",
+      anchors: ["Bottom", "Top"],
+      endpoint: "Blank",
+      connector: "Flowchart",
+    });
+    jsPlumb.connect({
+      source: "phongchucnang",
+      target: "p3",
+      anchors: ["Bottom", "Top"],
+      endpoint: "Blank",
+      connector: "Flowchart",
+    });
+    jsPlumb.connect({
+      source: "phongchucnang",
+      target: "p4",
+      anchors: ["Bottom", "Top"],
+      endpoint: "Blank",
+      connector: "Flowchart",
+    });
+    jsPlumb.connect({
+      source: "phongchucnang",
+      target: "p5",
+      anchors: ["Bottom", "Top"],
+      endpoint: "Blank",
+      connector: "Flowchart",
+    });
+  });
+});
