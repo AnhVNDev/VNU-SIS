a41a1b210395c3f6326e7f3ad334a18e7b6a13d3
anhvn <anhvn@anhvn.com>
2025-07-07 14:38:58 +0700

    update

--- javacsript.js
+++ javacsript.js
@@ -71,7 +71,7 @@ $(document).ready(function () {
     nav: false,
     dots: true,
     autoplay: true,
-    autoplayTimeout: 2000,
+    autoplayTimeout: 3000,
     autoplayHoverPause: true,
     responsive: {
       0: { items: 1 },
@@ -136,6 +136,20 @@ $(document).ready(function () {
       0: { items: 1 },
       600: { items: 1 },
       1000: { items: 6 },
+    },
+  });
+  $("#owl-carousel-quick-content").owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: { items: 1 },
+      600: { items: 1 },
+      1000: { items: 1 },
     },
   });
 });
